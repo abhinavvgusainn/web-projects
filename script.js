5db901dd2bde38f8a43c8866a6a5ff4c9a8a7ae3
Abhinav Gusain <232528803+abhinavvgusainn@users.noreply.github.com>
2025-12-29 21:20:41 +0530

    Introduced cursor and about section

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
-gsap.to("#nav",{
-    backgroundColor: "#000",
-    height: "100px",
-    duration: 0.5,
-    scrollTrigger:{
-        trigger:"#nav",
-        scroller:"body",
-        start: "bottom 10%",
-        end: "bottom 11%",
-        scrub: 2
-        
-    }
-})
+var cursor = document.querySelector("#cursor");
+var orb = document.querySelector("#orb");
+
+document.addEventListener("mousemove", function (dets) {
+  cursor.style.left = dets.x + "px";
+  cursor.style.top = dets.y + "px";
+  orb.style.left = dets.x - 150 + "px";
+  orb.style.top = dets.y - 150 + "px";
+});
+
+gsap.to("#nav", {
+  backgroundColor: "#000",
+  height: "100px",
+  duration: 0.5,
+  scrollTrigger: {
+    trigger: "#nav",
+    scroller: "body",
+    start: "bottom 10%",
+    end: "bottom 11%",
+    scrub: 2,
+  },
+});
+
+gsap.to("#main", {
+  backgroundColor: "#000",
+  scrollTrigger: {
+    trigger: "#main",
+    scroller: "body",
+    start: "top -25%",
+    end: "top -70%",
+    scrub: 2,
+  },
+});
