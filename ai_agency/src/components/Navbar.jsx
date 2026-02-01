b85da94d287a1aaf835306a01e8758b453d00f41
Abhinav Gusain <232528803+abhinavvgusainn@users.noreply.github.com>
2026-02-01 13:07:56 +0530

    added our-work section in ai_agency

--- ai_agency/src/components/Navbar.jsx
+++ ai_agency/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import ThemeToggleBtn from "./ThemeToggleBtn";
 
 const Navbar = ({theme, setTheme}) => {
 
-  const [sidebarOpen, setSidebarOpen] = useState(true);
+  const [sidebarOpen, setSidebarOpen] = useState(false);
   return (
     <div className="flex justify-between items-center px-4 sm:px-12 lg:px-24 xl:px-40 py-4 sticky top-0 z-20 backdrop-blur-xl font-medium bg-white/50 dark:bg-gray-900/70">
          
